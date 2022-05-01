a9e004d3f64e8f97db5c561c861efcfc2d9c22b9
iamramanavenkat <RAMANAIT14@gmail.com>
2022-05-01 10:18:50 +0530

    Added Ternary Operator

---
+++ CSharpBasics/TernaryOperator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TernaryOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            int c = 115;
+            string result = a > b ? "A is big" : b > c ? "B is big" : c > b ? "C is big" : "";
+            //Console.WriteLine(a > b ? "A is big" : "B is big");
+            //bool result = b > a ? true : false;
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
