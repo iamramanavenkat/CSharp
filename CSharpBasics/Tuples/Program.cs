50df8b67abd90a94f5115f326fad2b46b5a30b93
iamramanavenkat <RAMANAIT14@gmail.com>
2022-05-17 21:45:37 +0530

    PhoneNumberAnalysis.cs solution

---
+++ CSharpBasics/Tuples/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tuples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string msg = "All you need to know";
+            bool success = !string.IsNullOrWhiteSpace(msg);
+            (bool, int, string) triple = (success, 4, msg);
+            Console.WriteLine(triple.Item1);
+            Console.WriteLine(triple.Item2);
+            Console.WriteLine(triple.Item3);
+
+            //Initialization
+            (int, int) HeightWeight = (168, 55);
+
+            //Assignment
+            HeightWeight = (159, 52);
+
+            Console.WriteLine(HeightWeight.Item1 + "_" + HeightWeight.Item2);
+          
+            //Return value
+            (bool, int) GetSameOrBigger(int num1, int num2)
+            {
+                return (num1 == num2, num1 > num2 ? num1 : num2);
+            }
+
+            Console.WriteLine(GetSameOrBigger(25,12));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ExercismC#/PhoneNumberAnalysis_Tuples/PhoneNumber.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneNumberAnalysis_Tuples
+{
+    using System;
+
+
+
+    public static class PhoneNumber
+    {
+        public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber)
+        {
+            bool newYork = phoneNumber.Substring(0, 3) == "212" ? true : false;
+            //bool fake = phoneNumber.Substring(4,3) == "555" ? true : false;
+            bool Fake(string value)
+            {
+                bool fake = phoneNumber.Substring(4, 3) == "555" ? true : false;
+                return fake;
+            }
+            string locnumber = phoneNumber.Substring(8);
+            return (newYork, Fake(phoneNumber), locnumber);
+            throw new NotImplementedException($"Please implement the (static) PhoneNumber.Analyze() method");
+        }
+
+        public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo)
+        {
+            return phoneNumberInfo.IsFake;
+            throw new NotImplementedException($"Please implement the (static) PhoneNumber.IsFake() method");
+        }
+
+
+    }
+
+}
