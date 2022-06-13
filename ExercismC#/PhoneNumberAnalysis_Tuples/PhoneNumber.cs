ac1865c3ccee1495c794168b36a7f367b70011e7
iamramanavenkat <RAMANAIT14@gmail.com>
2022-06-13 21:07:24 +0530

    Added AssemblyLine_If_Numbers Exercise

--- ExercismC#/PhoneNumberAnalysis_Tuples/PhoneNumber.cs
+++ ExercismC#/PhoneNumberAnalysis_Tuples/PhoneNumber.cs
@@ -6,33 +6,19 @@ using System.Threading.Tasks;
 
 namespace PhoneNumberAnalysis_Tuples
 {
-    using System;
-
-
-
     public static class PhoneNumber
     {
         public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber)
-        {
-            bool newYork = phoneNumber.Substring(0, 3) == "212" ? true : false;
-            //bool fake = phoneNumber.Substring(4,3) == "555" ? true : false;
-            bool Fake(string value)
-            {
-                bool fake = phoneNumber.Substring(4, 3) == "555" ? true : false;
-                return fake;
-            }
-            string locnumber = phoneNumber.Substring(8);
-            return (newYork, Fake(phoneNumber), locnumber);
-            throw new NotImplementedException($"Please implement the (static) PhoneNumber.Analyze() method");
+        { 
+            string[] nums = phoneNumber.Split('-');
+            //Console.WriteLine(string.Join(",",nums));
+            return (nums[0] == "212", nums[1] == "555", nums[2]);
         }
-
         public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo)
         {
-            return phoneNumberInfo.IsFake;
-            throw new NotImplementedException($"Please implement the (static) PhoneNumber.IsFake() method");
+            return phoneNumberInfo.IsFake == true;
         }
-
-
     }
+
 
 }
